03f25ba3857f67aca42d01b86141b6adddbb344b
juriczech <bohdan.juricek@gmail.com>
2025-11-13 12:13:27 +0100

    chore(suite-native): toggle biometrics settings

--- suite-native/biometrics/src/useBiometricsSettings.ts
+++ suite-native/biometrics/src/useBiometricsSettings.ts
@@ -1,19 +1,11 @@
 import { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { isRejected } from '@reduxjs/toolkit';
 
 import { useAlert } from '@suite-native/alerts';
-import { events } from '@suite-native/analytics';
-import { useAnalytics } from '@suite-native/services';
 
-import {
-    authenticate,
-    selectIsBiometricsEnabled,
-    setIsBiometricsOverlayVisible,
-    setIsUserAuthenticated,
-    toggleEnableBiometrics,
-} from './biometricsSlice';
+import { ToggleBiometricsError, toggleBiometricsSettings } from './biometricsThunks';
 
 export type BiometricsToggleResult = 'enabled' | 'disabled' | 'failed' | 'notAvailable';
 
@@ -22,60 +14,28 @@ export const useBiometricsSettings = () => {
 
     const dispatch = useDispatch();
 
-    const isBiometricsEnabled = useSelector(selectIsBiometricsEnabled);
-    const analytics = useAnalytics();
     const toggleBiometricsOption = useCallback(async (): Promise<BiometricsToggleResult> => {
-        const authResult = await dispatch(authenticate()).unwrap();
+        const authResult = await dispatch(toggleBiometricsSettings());
 
-        if (isRejected(authResult) && authResult.payload === 'biometrics-not-available') {
-            await new Promise(resolve => {
-                showAlert({
-                    title: 'Biometrics',
-                    description:
-                        'No security features on your device. Make sure you have biometrics setup on your phone and try again.',
-                    primaryButtonTitle: 'Cancel',
-                    onPressPrimaryButton: () => resolve(undefined),
-                    pictogramVariant: 'warning',
+        if (isRejected(authResult)) {
+            if (authResult.payload === ToggleBiometricsError.NotAvailable) {
+                await new Promise(resolve => {
+                    showAlert({
+                        title: 'Biometrics',
+                        description:
+                            'No security features on your device. Make sure you have biometrics setup on your phone and try again.',
+                        primaryButtonTitle: 'Cancel',
+                        onPressPrimaryButton: () => resolve(undefined),
+                        pictogramVariant: 'warning',
+                    });
                 });
-            });
-
-            return 'notAvailable';
+            }
         }
 
-        if (!authResult?.success) {
-            return 'failed';
-        }
+        if (!authResult.payload) return 'failed';
 
-        dispatch(setIsBiometricsOverlayVisible(false));
-
-        if (isBiometricsEnabled) {
-            dispatch(toggleEnableBiometrics(false));
-            dispatch(setIsUserAuthenticated(false));
-            analytics.report({
-                type: events.biometricsChangeEvent.name,
-                payload: { enabled: false, origin: 'settingsToggle' },
-            });
-
-            return 'disabled';
-        }
-
-        setIsUserAuthenticated(true);
-        dispatch(toggleEnableBiometrics(true));
-        analytics.report({
-            type: events.biometricsChangeEvent.name,
-            payload: { enabled: true, origin: 'settingsToggle' },
-        });
-
-        return 'enabled';
-    }, [
-        analytics,
-        setIsBiometricsOptionEnabled,
-        setIsBiometricsOverlayVisible,
-        setIsUserAuthenticated,
-        showAlert,
-        dispatch,
-        isBiometricsEnabled
-    ]);
+        return authResult.payload;
+    }, [dispatch, showAlert]);
 
     return { toggleBiometricsOption };
 };
