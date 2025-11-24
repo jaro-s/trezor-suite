59020bacd117e3cb0b1b7bbb841a1a8d81c9697c
juriczech <bohdan.juricek@gmail.com>
2025-11-24 12:19:27 +0000

    chore(suite-native): migration

--- suite-native/state/src/reducers.ts
+++ suite-native/state/src/reducers.ts
@@ -59,6 +59,7 @@ import {
     migrateAccountLabel,
     migrateAccountsDeprecateNetworks,
     migrateAutoEjectToWalletSettings,
+    migrateBiometricsAtomToRedux,
     migrateDeviceState,
     migrateLocaleTagToAppLocaleCode,
     migrateTransactionsBnbToBsc,
@@ -123,6 +124,9 @@ export const prepareRootReducers = (deps: PrepareRootReducersDeps) => {
         persistedKeys: biometricsPersistWhitelist,
         key: biometricsSlice.name,
         version: 1,
+        migrations: {
+            1: migrateBiometricsAtomToRedux,
+        },
     });
 
     const tradingPersistedReducer = preparePersistReducer({
