269170c42b91ea75862d7ce1986a118bd599d2c4
Mattew Husak <matej.husak@icloud.com>
2026-04-28 15:08:39 +0200

    refactor(nfc): restructure folder layout by domain

--- suite/nfc/src/create-backup/NoNfcTags.tsx
+++ suite/nfc/src/create-backup/NoNfcTags.tsx
@@ -2,7 +2,7 @@ import { Translation } from '@suite/intl';
 import { OnboardingCard } from '@suite/onboarding-components';
 import { Row } from '@trezor/components';
 
-import { BackupAlternativeCard } from './BackupAlternativeCard';
+import { BackupAlternativeCard } from '../components/BackupAlternativeCard';
 
 interface NoNfcTagsProps {
     onFinishSetup: () => void;
