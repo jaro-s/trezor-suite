c85c00e3eaa736322a22b43bdb7db34a42a05ace
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-04-29 18:21:15 +0200

    TESTING

--- packages/connect/src/data/config.ts
+++ packages/connect/src/data/config.ts
@@ -156,7 +156,7 @@ export const config: Config = {
                 T2B1: '0',
                 T3B1: '0',
                 T3T1: '0',
-                T3W1: '2.11.2',
+                T3W1: '2.11.1',
             },
         },
         {
