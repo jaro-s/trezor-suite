56961e7fa3faf3dac53cc9e7cded25b5c73d0a07
Martin Varmuza <mvarmuza@gmail.com>
2026-05-01 18:15:51 +0200

    refactor(suite-native): remove redundant trading state types

--- suite-native/trading-types/src/state.ts
+++ suite-native/trading-types/src/state.ts
@@ -1,9 +1,6 @@
 import type { ProviderMetadata } from 'invity-api';
 
 import type {
-    TradingBuyState as CommonTradingBuyState,
-    TradingExchangeState as CommonTradingExchangeState,
-    TradingSellState as CommonTradingSellState,
     TradingState as CommonTradingState,
     InvityServerEnvironment,
     TradingCountryCode,
@@ -11,12 +8,6 @@ import type {
 } from '@suite-common/trading';
 
 import type { ProviderConfirmationStatus } from './general';
-
-export interface TradingBuyState extends CommonTradingBuyState {}
-
-export interface TradingExchangeState extends CommonTradingExchangeState {}
-
-export interface TradingSellState extends CommonTradingSellState {}
 
 export type TradingResidenceState = {
     country: TradingCountryCode | undefined;
@@ -32,9 +23,6 @@ export type TradingResidenceRootState = {
 };
 
 export interface TradingState extends CommonTradingState {
-    buy: TradingBuyState;
-    exchange: TradingExchangeState;
-    sell: TradingSellState;
     residence: TradingResidenceState;
     tradingEnvironment: InvityServerEnvironment;
     tradeOrderIdToBeOpened: string | undefined;
